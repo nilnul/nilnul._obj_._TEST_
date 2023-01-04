2fca175c8f43d7f778ffcc36510ddb2d7fd883eb
WANG,Youtian <wangyoutian@nilnul.com>
2023-01-04 22:19:40 +0800

    dynamic vs object arg into a generic method reading typeof(T);

---
+++ nilnul0/_obj/_typ/gener/typof/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace nilnul._obj_._TEST_.nilnul0._obj._typ.gener.typeof0
+{
+	[TestClass]
+	public class UnitTest11
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			UntypedMethod("hi");
+			UntypedMethod(123);
+
+
+
+			Assert.IsFalse(
+				nilnul.obj.str.Eq<string>.Singleton.Equals(
+					list, new[] { typeof(string).Name, typeof(int).Name }
+				)
+			);
+		}
+
+		static void UntypedMethod(object obj)
+		{
+			//dynamic t = obj;
+			GenerMethod(obj);
+		}
+
+		static List<string> list = new List<string>();
+
+		static void GenerMethod<T>(T item)
+		{
+			var typ = typeof(T);
+
+			Debug.WriteLine(typeof(T).FullName);
+			list.Add(typ.Name);
+
+		}
+	}
+}
---
+++ nilnul0/_obj/_typ/gener/typof/dyna/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace nilnul._obj_._TEST_.nilnul0._obj._typ.gener.typof.dyna
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			UntypedMethod("hi");
+			UntypedMethod(123);
+
+			Assert.IsTrue(
+				nilnul.obj.str.Eq<string>.Singleton.Equals(
+					list, new[] { typeof(string).Name,typeof(int).Name }
+				)
+			);
+		}
+
+		static void UntypedMethod(object obj) {
+			dynamic t = obj;
+			GenerMethod(t);
+		}
+
+		static List<string> list = new List<string>();
+
+		static void GenerMethod<T>(T item) {
+
+			Debug.WriteLine(typeof(T).FullName);
+			list.Add(typeof(T).Name);
+			
+		}
+	}
+}
